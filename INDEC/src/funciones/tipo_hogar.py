17e16613a90ad51c8f4e2ad5d32fdd554db6ef9a
Diego Arrechea <diego.arrechea.unlp@gmail.com>
2025-05-09 11:26:46 -0300

    Se agrego la funcion key_tipo_hogar, se importo en el notebook hogares y se explico su funcionamiento.

---
+++ INDEC/src/funciones/tipo_hogar.py
@@ -0,0 +1,13 @@
+def key_tipo_hogar(list_dic_hogar):
+    for dic in list_dic_hogar:
+        cant_personas = int(dic.get('IX_TOT', 0))
+        if cant_personas == 1:
+            dic['TIPO_HOGAR'] = 'Unipersonal'
+        elif 2 <= cant_personas <= 4:
+            dic['TIPO_HOGAR'] = 'Nuclear'
+        elif cant_personas >= 5:
+            dic['TIPO_HOGAR'] = 'Extendido'
+        else:
+            dic['TIPO_HOGAR'] = 'Sin Datos'
+
+    return list_dic_hogar
