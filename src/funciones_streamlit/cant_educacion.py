7bbc6d3113873fffffa0eb1698be82bafa721b9c
Lautaro Loredo <lautaroloredo5@gmail.com>
2025-06-01 15:23:01 -0300

    finalizo el punto 1.6.1 y agrego a src/streamlit un archivo

---
+++ src/funciones_streamlit/cant_educacion.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from pathlib import Path
+import streamlit as st
+
+@st.cache_data
+def carga_df ():
+
+    ruta_csv_individuos = Path(__file__).parent.parent.parent / "utils" / "IndividuosTotal.csv"
+    
+    columnas_necesarias = ["ANO4", "TRIMESTRE", "NIVEL_ED_str", "PONDERA"]
+    df = pd.read_csv(ruta_csv_individuos, delimiter=";", usecols=columnas_necesarias)
+    
+    # Chequeo que las columnas esten
+    columnas = ["ANO4", "TRIMESTRE",  "NIVEL_ED_str", "PONDERA"]
+    if not set(columnas).issubset(df.columns):
+        st.error("El archivo no contiene las columnas necesarias")
+        st.stop()
+    return df
+
+def anio_trimestre (df):
+    
+    
+    # Seleccion de anio-trimestre
+    anios_disponibles = sorted(df["ANO4"].unique())
+    anios_opciones = ["Seleccione un año..."] + list(map(int,anios_disponibles))
+
+    anio_seleccionado = st.selectbox("Seleccione un año:", anios_opciones)
+
+    trimestre_seleccionado = None
+    if anio_seleccionado != "Seleccione un año...":
+        trimestres_disponibles = sorted(df[df["ANO4"] == int(anio_seleccionado)]["TRIMESTRE"].unique())
+        trimestres_opciones = ["Seleccione un trimestre..."] + list(map(int,trimestres_disponibles))
+
+        trimestre_seleccionado = st.selectbox("Seleccione un trimestre:", trimestres_opciones)
+
+    if trimestre_seleccionado != "Seleccione un trimestre...":
+        return anio_seleccionado, trimestre_seleccionado
+    else:
+        return anio_seleccionado, None
+
+    
+def personalizacion_datos(df):
+    
+    # Seleccion de anio-trimestre
+    anio, trimestre = anio_trimestre(df)
+
+    df = df[(df["ANO4"] == anio) & (df["TRIMESTRE"] == trimestre)]
+    
+    # Agrupo y sumo Ponderaciones
+    df_resumen = (df.groupby('NIVEL_ED_str')['PONDERA'].sum().reset_index())
+    # Ordeno
+    df_resumen = df_resumen.sort_values(by='PONDERA', ascending=True)   
+    #Elimino dato no relevante     
+    df_resumen = df_resumen[df_resumen["NIVEL_ED_str"] != "Sin informacion"]\
+    # Cambios nombres de las columnas 
+    cambios_nom_columns = {'NIVEL_ED_str':'Niveles Educativos', 'PONDERA' : 'Cantidad Maxima'}
+    df_final = df_resumen.rename(columns = cambios_nom_columns)
+    
+    return df_final
+    
--- streamlit/pages/6_Educacion.py
+++ streamlit/pages/6_Educacion.py
@@ -2,83 +2,27 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
+import sys
+
+project_root = Path(__file__).parent.parent.parent/'src'
+sys.path.append(str(project_root))
+
+import funciones_streamlit.cant_educacion as ced
 
 st.title("🧑‍🎓📚️ Educacion")
 
 st.info("""En esta sección se visualizará información relacionada al nivel de educación
         alcanzado por la población argentina según la EPH.""")
-
-ruta_csv_individuos = Path(__file__).parent.parent.parent / "utils" / "IndividuosTotal.csv"
 st.divider()
 
+
 # Carga de datos
+df = ced.carga_df()
+df_final = ced.personalizacion_datos(df)
 
-def cargar_csv():
-    df = pd.read_csv(ruta_csv_individuos, delimiter=";")
-    columnas = ["ANO4", "NIVEL_ED_str", "PONDERA", "CODUSU", "NRO_HOGAR", "COMPONENTE"]
-    df = df[columnas]
-    return df
-
-df = cargar_csv()
-
-columnas = ["ANO4", "NIVEL_ED_str", "PONDERA", "CODUSU", "NRO_HOGAR", "COMPONENTE"]
-if not set(columnas).issubset(df.columns):
-    st.error("El archivo no contiene las columnas necesarias")
-    st.stop()
-
-anios_disponibles = sorted(df["ANO4"].unique())
-anios = st.selectbox("Seleccione un año: ", anios_disponibles)
-
-#Averiguo los datos para las educaciones maximas por año
-st.write (df.shape)
-
-nivel_orden = {
-    "Primario incompleto": 1,
-    "Primario completo": 2,
-    "Secundario incompleto": 3,
-    "Secundario completo": 4,
-    "Terciario/Universitario incompleto": 5,
-    "Terciario/Universitario completo": 6,
-}
-df['nivel_num'] = df['NIVEL_ED_str'].map(nivel_orden)
-
-# Quedarse con la entrada de nivel educativo más alto por persona y año
-df_nivel_max = df.sort_values('nivel_num', ascending=False).drop_duplicates(
-    subset=['ANO4', 'CODUSU', 'NRO_HOGAR', 'COMPONENTE']
-)
-
-df_sin_duplicados = df.drop_duplicates(subset=['ANO4', 'CODUSU', 'NRO_HOGAR', 'COMPONENTE'])
-df_resumen = (df_nivel_max.groupby(['ANO4', 'NIVEL_ED_str'])['PONDERA'].sum().reset_index())
-
-# Ordenar columnas
-df_final = df_resumen.groupby('NIVEL_ED_str')['PONDERA'].sum().reset_index()
-df_final = df_final.sort_values(by='PONDERA', ascending=True)
-st.title("Resumen educativo anual (EPH)")
-st.dataframe(df_final)
-
-
-# conteo_de_niveles = df["NIVEL_ED_str"].value_counts() 
-
-# maximo_nivel_primario = conteo_de_niveles[["Superior o universitario","Secundario incompleto","Secundario completo","Primario completo"]].sum()
-# st.write(maximo_nivel_primario)
-
-# maximo_nivel_secundario = conteo_de_niveles[["Superior o universitario","Secundario completo"]].sum()
-# st.write(maximo_nivel_secundario)
-
-# maximo_nivel_superior_universitario = conteo_de_niveles[["Superior o universitario"]].sum()
-# st.write(maximo_nivel_superior_universitario)
-
-# claves = ('max_lvl_primario', 'max_lvl_secundario', 'max_lvl_universitario')
-# comparaciones = [maximo_nivel_primario, maximo_nivel_secundario, maximo_nivel_superior_universitario]
-
-# fig,ax = plt.subplots(figsize=(10,10))
-
-# ax.pie(comparaciones, labels=claves, autopct='%1.2f%%', startangle= 90)
-
-# # Agregar título
-# plt.title('Maximos Niveles Educativos')
-
-# ax.axis('equal')
-
-# # Mostrar el gráfico de torta
-# st.pyplot(fig)
+if df_final is not None and not df_final.empty:
+    st.title("Resumen educativo Trimestral")
+    df_final = df_final.set_index("Niveles Educativos")
+    st.table(df_final)
+else:
+    st.warning("Por favor seleccione un año y trimestre válido para ver el resumen.")
