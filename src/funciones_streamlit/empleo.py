028f31f1c369a2fdbe8ca5c85d8cd52a361b25f8
Ulises Rodriguez <ulises25103@gmail.com>
2025-06-03 21:31:24 -0300

    punto 1.5.1

--- src/funciones_streamlit/empleo.py
+++ src/funciones_streamlit/empleo.py
@@ -3,15 +3,15 @@ from pathlib import Path
 import streamlit as st
 
 @st.cache_data
-def cargar_df():
-
-    ruta_df = Path(__file__).parent.parent.parent / "utils" / "IndividuosTotal.csv"
+def cargar_df(ruta_df):
 
     COLUMNAS_NECESARIAS = ["ESTADO", "NIVEL_ED", "ANO4", "TRIMESTRE", "AGLOMERADO", "PP04A"]
     try:
         df = pd.read_csv(ruta_df,delimiter=";", usecols=COLUMNAS_NECESARIAS)
 
         if not set(COLUMNAS_NECESARIAS).issubset(df.columns):
+            st.error("El archivo no contiene las columnas necesarias")
+            st.stop()
             raise ValueError("El archivo no contiene las columnas necesarias.")
         else:
             return df
@@ -25,7 +25,37 @@ def cargar_df():
         print(f"Error al cargar el archivo CSV, ERROR: ", {str(e)})
         return None
 
-NIVEL_EDUCATIVO = "NIVEL_ED"
 TIPO_EMPLEO = "PP04A"
 ESTADO_LABORAL = "ESTADO"
-# 1.5.1: ESTADO = 2, CH12 = 1-9, ANO4 = input, TRIMESTRE= input
+
+# Cargar datos ➔ 2. Filtrar año-trimestre ➔ 3. Filtrar desocupados ➔ 4. Agrupar por educación ➔ 5. Contar ponderado ➔ 6. Mostrar.
+
+def definir_año():
+    ruta_df = Path(__file__).parent.parent.parent / "utils" / "IndividuosTotal.csv"
+    df = cargar_df(ruta_df)
+
+    anios_disponibles = sorted(df["ANO4"].unique())
+    anios_opciones = ["Seleccione un año..."] + list(map(int, anios_disponibles))
+
+    anio_seleccionado = st.selectbox("Seleccione un año:", anios_opciones)
+    trimestre_seleccionado = None
+    if anio_seleccionado != "Seleccione un año...":
+        trimestres_disponibles = sorted(df[df["ANO4"] == int(anio_seleccionado)]["TRIMESTRE"].unique())
+        trimestres_opciones = ["Seleccione un trimestre..."] + list(map(int,trimestres_disponibles))
+
+        trimestre_seleccionado = st.selectbox("Seleccione un trimestre:", trimestres_opciones)
+
+    if trimestre_seleccionado != "Seleccione un trimestre...":
+        return anio_seleccionado, trimestre_seleccionado
+    else:
+        return anio_seleccionado, None
+
+# 1.5.1
+def calcular_desocupados_por_nivel(df, anio, trimestre, NIVEL_EDUCATIVO):
+    df_filtrado = df[(df["ANO4"] == anio) & (df["TRIMESTRE"] == trimestre)]
+    df_desocupados = df_filtrado[(df_filtrado[ESTADO_LABORAL] == 2)]
+    # cambio los numeros que representan cada nivel educativo por su descripcion
+    df_desocupados["NIVEL_ED"] = df_desocupados["NIVEL_ED"].map(NIVEL_EDUCATIVO)
+    # Esto cuenta cuántos desocupados hay para cada valor de NIVEL_ED (del 1 al 9):
+    conteo = df_desocupados.groupby("NIVEL_ED").size().sort_index()
+    return conteo
--- streamlit/pages/5_Actividad_y_Empleo.py
+++ streamlit/pages/5_Actividad_y_Empleo.py
@@ -6,12 +6,35 @@ import matplotlib.pyplot as plt
 
 from datetime import datetime as dt
 
-ruta_utils = Path(__file__).parents[2] / "utils"
+project_root = Path(__file__).parent.parent.parent
+sys.path.append(str(project_root))
+ruta_csv_individuos = Path(__file__).parent.parent.parent / "utils" / "IndividuosTotal.csv"
+
+import funciones_streamlit.empleo as emp
+import utils.constantes as cons
+
 
 st.set_page_config(layout="wide")
-st.title("💼 Actividad y Empleo")
+st.title("💼⚙️ Actividad y Empleo")
 st.info("En esta sección se visualizará información relacionada a la actividad y empleo según la EPH.")
 
 st.divider()
-# 1.5.1 Para las personas desocupadas, informar la cantidad de ellas según sus estudios alcanzados. Se debe informar para un año y trimestre elegido por el usuario
+st.markdown(
+    "<h2 style='text-align: center;'>Informacion de personas desocupadas segun sus estudios alcanzados.</h2>",
+    unsafe_allow_html=True,
+)
 
+# cargo datos
+df = emp.cargar_df(ruta_csv_individuos)
+
+# 1.5.1 Para las personas desocupadas, informar la cantidad de ellas según sus estudios alcanzados. Se debe informar para un año y trimestre elegido por el usuario
+anio, trimestre = emp.definir_año()
+if anio and trimestre:
+    conteo = emp.calcular_desocupados_por_nivel(df, anio, trimestre, cons.NIVEL_EDUCATIVO)
+    fig, ax = plt.subplots()
+    ax.pie(conteo, labels=conteo.index, autopct="%1.1f%%")
+    ax.set_title("Distribución de desocupados según nivel educativo")
+    st.pyplot(fig)
+else:
+    st.warning('Por favor, elija año y trimestre')
+st.divider()
