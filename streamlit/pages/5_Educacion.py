d0d9c54a7e1a41ec3912557eb20f0d46325d45f9
Lautaro Loredo <lautaroloredo5@gmail.com>
2025-06-08 01:03:03 -0300

    modificacion visual y de detalles. Se agregan librerias al requirements

--- streamlit/pages/5_Educacion.py
+++ streamlit/pages/5_Educacion.py
@@ -8,8 +8,8 @@ sys.path.append(str(project_root))
 
 
 import funciones_streamlit.educacion as ed
-import consultas.ranking5 as r5
-import consultas.consulta_leer_escribir as le
+from consultas.ranking5 import ranking_aglomerados_nivel_sup
+from consultas.consulta_leer_escribir import calcular_porcentajes_lectura
 
 st.title("🧑‍🎓📚️ Educacion")
 
@@ -24,12 +24,14 @@ if st.session_state.get("datos_actualizados", False):
 
 # Carga de datos
 df = ed.carga_df()
+
+st.title("Informacion educativa Trimestral")
+
 df_trimestral,df_por_anio = ed.personalizacion_datos(df)
 
 # Hasta que el usuario no seleccione un año y trimestre no se va a visualizar ni ejecutar nada de la pagina
 if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
-    st.title("Resumen educativo Trimestral")
-    st.write(' Informacion la cantidad maxima de personas que terminaron o no un nivel educativo')
+    st.write(' Informa la cantidad maxima de personas que terminaron o no un nivel educativo')
     df_trimestral = df_trimestral.set_index("Niveles Educativos")
     st.table(df_trimestral)
 
@@ -38,7 +40,8 @@ if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
 
     orden_etario = ["20-30", "30-40", "40-50", "50-60", "+60"]
 
-    st.write(' Informacion sobre nivel educacional mas comun entre grupos etarios de 10 en 10')
+    st.title('Resumen sobre nivel educacional')
+    st.text(' En esta seccion respecto al año seleccionado anteriormente se informa el nivel educativo mas comun entre la poblacion separados por grupos etario de pares de 10 en 10')
     seleccionar_todos = st.checkbox("Seleccionar todos los grupos etarios")
     if seleccionar_todos:
         seleccion = orden_etario  
@@ -57,7 +60,7 @@ if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
         # creo el grafico para visualizar lo obtenido en resultados_por_grupos
         fig = ed.grafico_barras(resultados_por_grupos, orden_etario)
         
-        st.plotly_chart(fig)
+        st.plotly_chart(fig, key="fig")
 
     else:
         st.info("Por favor seleccione al menos un grupo etario.")
@@ -70,7 +73,7 @@ if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
     universitarios o superiores finalizados''')
 
     # Llamo a la funcion ranking 5 
-    data = r5.ranking_englomerados_nivelSup()
+    data = ranking_aglomerados_nivel_sup()
 
     # le paso "data" que contiene el diccionario con el top 5 aglomerados y me lo devuelve a csv
     csv = ed.exportar_csv(data)
@@ -86,7 +89,7 @@ if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
     st.divider() # Aca arranca el punto 1.6.4
     
     # Llamo a la consulta leer_escribir
-    años, porcen_sabe, porcen_nosabe = le.calcular_porcentajes_lectura()
+    años, porcen_sabe, porcen_nosabe = calcular_porcentajes_lectura()
     
     # Muestro los porcentajes en formato texto
     for i in range(len(años)):
@@ -96,9 +99,8 @@ if isinstance(df_trimestral, pd.DataFrame) and not df_trimestral.empty:
         st.markdown("---")
 
     # llamo al grafico creado y lo dejo en segundo plano
-    with st.expander("📈 Ver gráfico de lectura por año (preciso)"):
-        chart = ed.grafica_porcentajes_lectura(años, porcen_sabe, porcen_nosabe)
-        st.altair_chart(chart, use_container_width=True)
+    chart = ed.grafica_porcentajes_lectura(años, porcen_sabe, porcen_nosabe)
+    st.plotly_chart(chart, use_container_width=True, key="lectura_chart")
 else:
     st.warning("Por favor seleccione un año y trimestre válido para ver el resumen.")
 
