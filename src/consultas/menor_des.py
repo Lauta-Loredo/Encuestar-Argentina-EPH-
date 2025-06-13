98d046d70ba795bec93ae4ee4c264d6b8cc3e5c7
Ulises Rodriguez <ulises25103@gmail.com>
2025-06-13 12:44:36 -0300

    agrego ponderacion en punto 3 seccion B

--- src/consultas/menor_des.py
+++ src/consultas/menor_des.py
@@ -2,7 +2,7 @@
 import csv
 from pathlib import Path
 
-
+ANIO_REGISTRO = 'ANO4'
 def menor_des():
     desocupacion = {}
 
@@ -11,17 +11,18 @@ def menor_des():
     with open(ruta_individuos, mode='r', encoding='utf-8') as archivo:
         lector = csv.DictReader(archivo, delimiter=";")
         for persona in lector:
-            anio = persona['ANO4']
+            anio = persona[ANIO_REGISTRO]
             trimestre = persona['TRIMESTRE']
             estado = persona['ESTADO']
+            ponderacion = int(persona['PONDERA'])
             clave = (anio, trimestre)
 
             if clave not in desocupacion:
                 desocupacion[clave] = {'desocupados': 0, 'total': 0}
 
-            desocupacion[clave]['total'] += 1
+            desocupacion[clave]['total'] += ponderacion
             if estado == '2':  
-                desocupacion[clave]['desocupados'] += 1
+                desocupacion[clave]["desocupados"] += ponderacion
 
     resultados = []
 
