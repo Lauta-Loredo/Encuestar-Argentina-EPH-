12dd2c2f10f4933c7ac42632b3f7b10053961eed
Axel Morano <axelmorano@Axels-MBP.fibertel.com.ar>
2025-06-18 00:06:38 -0300

    Se hicieron las importaciones y se modifico la creacion del DF

--- streamlit/pages/3_Características_de_la_Vivienda.py
+++ streamlit/pages/3_Características_de_la_Vivienda.py
@@ -23,8 +23,7 @@ from src.funciones_streamlit.funciones_en_comun import (
     crear_dataframe,
     selector_anios,
     filtrar_dataframe_por_anio,
-    footer,
-
+    footer
 )
 
 reload(viviendas) 
@@ -32,22 +31,40 @@ reload(viviendas)
 # Ahora vuelves a importar lo que necesitas para que sean accesibles con las funciones recargadas:
 from src.funciones_streamlit.viviendas import (
     calcular_cantidad,
+    calcular_proporcion_tipo_viviendas,
     grafico_tipo_de_viviendas,
-    material_predominante_por_aglomerado,
-    proporcion_viviendas_banio,
-    evolucion_tenencia,
-    cantidad_viviendas_en_villas,
-    condiciones_de_habitabilidad,
+    calcular_material_predominante_por_aglomerado,
+    informar_material_predominante,
+    calcular_proporcion_viviendas_banio,
+    informar_prop_banio_interior,
+    informar_evolucion_tenencia,
+    calcular_cantidad_viviendas_en_villas,
+    informar_viviendad_en_villas,
+    calcular_condicion_de_habitabilidad,
+    informar_cond_habitabilidad
 )
 
-df_hogares = crear_dataframe()
+from utils.constantes import HOGARES_CSV
+# Determina las columnas necesarias para el DF, asi no se sobrecarga la memoria
+columnas_necesarias = ['ANO4','TRIMESTRE','PONDERA','AGLOMERADO','CONDICION_DE_HABITABILIDAD','IV1','IV3','IV9','IV12_3','II7']
 
+# Invoca a la función y retorna el DF
+df_hogares = crear_dataframe(HOGARES_CSV,columnas_necesarias)
+
+# Verifica que el DF no este vacio
 if df_hogares is not  None:
-    anio_seleccionado = selector_anios(df_hogares)
+    
+    # Selectbox para seleccionar un año
+    anio_seleccionado = selector_anios(df_hogares,True)
+    
+    # Cuando el usuario selecciono el año, se filtra por el mismo
     if anio_seleccionado != 'Seleccione un año...':
         df = filtrar_dataframe_por_anio(df_hogares,anio_seleccionado)
+        
+        # Muestra la cantidad total de hogares en el periodo seleccionado
         calcular_cantidad(df,"**Cantidad de hogares en el periodo seleccionado**:")
-    
+        
+        # Divide la pagina en tabs, mejora el orden
         tabs = st.tabs(['Tipos de Viviendas',
                                 'Material de piso predominante por aglomerado',
                                 'Prop. viviendas con baño dentro del hogar',
@@ -57,22 +74,22 @@ if df_hogares is not  None:
                                 ])
         
         with tabs[0]:
-            grafico_tipo_de_viviendas(df)
+            grafico_tipo_de_viviendas(calcular_proporcion_tipo_viviendas(df))
         
         with tabs[1]:
-            material_predominante_por_aglomerado(df)
+            informar_material_predominante(calcular_material_predominante_por_aglomerado(df))
 
         with tabs[2]:
-            proporcion_viviendas_banio(df)
+            informar_prop_banio_interior(calcular_proporcion_viviendas_banio(df))
         
         with tabs[3]:
-            evolucion_tenencia(df)
+            informar_evolucion_tenencia(df)
         
         with tabs[4]:
-            cantidad_viviendas_en_villas(df)
+            informar_viviendad_en_villas(calcular_cantidad_viviendas_en_villas(df))
         
         with tabs[5]:
-            condiciones_de_habitabilidad(df)
+            informar_cond_habitabilidad(calcular_condicion_de_habitabilidad(df))
     else: 
         st.warning('Seleccione un periodo para poder trabajar con el dataframe.')
 
