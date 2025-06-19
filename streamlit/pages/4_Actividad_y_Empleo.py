7b9431ae7137cb1107c029356dc5ccb58282c70f
Ulises Rodriguez <ulises25103@gmail.com>
2025-06-18 21:41:31 -0300

    Modificaciones en pagina 4: Agregue un toggle elegir si visualizar los datos o no. Puse un expander para el punto 1.5.4. Y agregue una funcionalidad en el punto 1.5.5 donde si no elijo ni empleo ni desempleo no muestra el mapa.

--- streamlit/pages/4_Actividad_y_Empleo.py
+++ streamlit/pages/4_Actividad_y_Empleo.py
@@ -43,11 +43,6 @@ st.info(
     "En esta sección se visualizará información relacionada a la actividad y empleo según la EPH."
 )
 
-st.divider()
-st.markdown(
-    "<h2 style='text-align: center;'>Información de personas desocupadas segun sus estudios alcanzados.</h2>",
-    unsafe_allow_html=True,
-)
 
 if st.session_state.get("datos_actualizados", False):
     st.cache_data.clear()  # fuerza recarga
@@ -62,72 +57,86 @@ COLUMNAS_NECESARIAS = [
     "PP04A",
     "PONDERA",
 ]
-
-# cargo datos
-df = crear_dataframe(ruta_csv_individuos,COLUMNAS_NECESARIAS)
-
-# 1.5.1 Para las personas desocupadas, informar la cantidad de ellas según sus estudios alcanzados. Se debe informar para un año y trimestre elegido por el usuario
-anio = definir_anio(df, "1.5.1A")
-trimestre = definir_trimestre(df, anio, "1.5.1T")
-if anio and trimestre:
-    conteo = calcular_desocupados_por_nivel(df, anio, trimestre, cons.NIVEL_EDUCATIVO)
-    fig, ax = plt.subplots()
-    ax.pie(conteo, labels=conteo.index, autopct="%1.1f%%")
-    ax.set_title("Distribución de desocupados según nivel educativo")
-    st.pyplot(fig)
-else:
-    st.warning("Por favor, elija año y trimestre")
 st.divider()
+mostrar_graficos = st.toggle("***Visualizar Datos Sobre Actividad y Empleo***", value=False)
+if mostrar_graficos:
+    # cargo datos
+    df = crear_dataframe(ruta_csv_individuos,COLUMNAS_NECESARIAS)
+    st.markdown(
+        "<h2 style='text-align: center;'>Información de personas desocupadas segun sus estudios alcanzados.</h2>",
+        unsafe_allow_html=True,
+    )
+    # 1.5.1 Para las personas desocupadas, informar la cantidad de ellas según sus estudios alcanzados. Se debe informar para un año y trimestre elegido por el usuario
+    anio = definir_anio(df, "1.5.1A")
+    trimestre = definir_trimestre(df, anio, "1.5.1T")
+    if anio and trimestre:
+        conteo = calcular_desocupados_por_nivel(df, anio, trimestre, cons.NIVEL_EDUCATIVO)
+        fig, ax = plt.subplots()
+        ax.pie(conteo, labels=conteo.index, autopct="%1.1f%%")
+        ax.set_title("Distribución de desocupados según nivel educativo")
+        st.pyplot(fig)
+    else:
+        st.warning("Por favor, elija año y trimestre")
+    st.divider()
 
-# 1.5.2 Informar la evolución del desempleo(tasa de desempleo) a lo largo del tiempo. Se debe poder filtrar por aglomerado y en caso de no elegir ninguno se debe calcular para todo el país.
-st.markdown(
-    "<h2 style='text-align: center;'>Evolucion de la tasa de desempleo segun aglomerado o país.</h2>",
-    unsafe_allow_html=True,
-)
-tipo = "desempleo"
-aglomerado = definir_aglomerado(df, "1.5.31", cons.NOMBRES_AGLOMERADOS)
-evolucion = tasa_des_empleo(df, tipo, aglomerado)
-muestra = muestra_tasa(tipo, evolucion)
-st.divider()
+    # 1.5.2 Informar la evolución del desempleo(tasa de desempleo) a lo largo del tiempo. Se debe poder filtrar por aglomerado y en caso de no elegir ninguno se debe calcular para todo el país.
+    st.markdown(
+        "<h2 style='text-align: center;'>Evolucion de la tasa de desempleo segun aglomerado o país.</h2>",
+        unsafe_allow_html=True,
+    )
+    tipo = "desempleo"
+    aglomerado = definir_aglomerado(df, "1.5.31", cons.NOMBRES_AGLOMERADOS)
+    evolucion = tasa_des_empleo(df, tipo, aglomerado)
+    muestra = muestra_tasa(tipo, evolucion)
+    st.divider()
 
-# 1.5.3 Informar la evolución del empleo(tasa de empleo) a lo largo del tiempo. Se debe poder filtrar por aglomerado y en caso de no elegir ninguno se debe calcular para todo el país.
-st.markdown(
-    "<h2 style='text-align: center;'>Evolucion de la tasa de empleo segun aglomerado o país.</h2>",
-    unsafe_allow_html=True,
-)
-tipo = "empleo"
-aglomerado = definir_aglomerado(df, "1.5.32", cons.NOMBRES_AGLOMERADOS)
-evolucion = tasa_des_empleo(df, tipo, aglomerado)
-muestra2 = muestra_tasa(tipo, evolucion)
-st.divider()
+    # 1.5.3 Informar la evolución del empleo(tasa de empleo) a lo largo del tiempo. Se debe poder filtrar por aglomerado y en caso de no elegir ninguno se debe calcular para todo el país.
+    st.markdown(
+        "<h2 style='text-align: center;'>Evolucion de la tasa de empleo segun aglomerado o país.</h2>",
+        unsafe_allow_html=True,
+    )
+    tipo = "empleo"
+    aglomerado = definir_aglomerado(df, "1.5.32", cons.NOMBRES_AGLOMERADOS)
+    evolucion = tasa_des_empleo(df, tipo, aglomerado)
+    muestra2 = muestra_tasa(tipo, evolucion)
+    st.divider()
 
-# 1.5.4
-st.markdown(
-    "<h2 style='text-align: center;'>Informacion por aglomerado sobre personas ocupadas y su tipo de ocupación.</h2>",
-    unsafe_allow_html=True,
-)
+    # 1.5.4
+    st.markdown(
+        "<h2 style='text-align: center;'>Informacion por aglomerado sobre personas ocupadas y su tipo de ocupación.</h2>",
+        unsafe_allow_html=True,
+    )
+    with st.expander("📊 Mostrar analisis por aglomerado"):
+        df_resultado = ocupados_por_nivel(df)  
+        st.dataframe(df_resultado)
+    st.divider()
 
-df_resultado = ocupados_por_nivel(df)  
-st.dataframe(df_resultado)
-st.divider()
-# 1.5.5
-datos_path = cons.DATA_PATH / "aglomerados_coordenadas.json"
-with open(datos_path, "r", encoding="utf-8") as f:
-    coordenadas_aglomerado = json.load(f)
+    # 1.5.5
+    datos_path = cons.DATA_PATH / "aglomerados_coordenadas.json"
+    with open(datos_path, "r", encoding="utf-8") as f:
+        coordenadas_aglomerado = json.load(f)
 
-tasas = tasa_aglomerado(df)
-tipo = st.selectbox("Seleccioná el tipo de tasa que querés visualizar:", ("empleo", "desempleo"))
-st.markdown(
-    "<p>- Al elegir la tasa de empleo se ven puntos verdes en los aglomerados cuya tasa de empleo aumentó con el correr del tiempo. Rojo en el caso contrario. </p>"
-    "<p>- Al elegir la tasa de desempleo se ven puntos rojos en los aglomerados cuya"
-    "tasa de empleo aumentó con el correr del tiempo. Verde en el caso contrario. </p>",
-    unsafe_allow_html=True,
-)
-colores = colores_aglomerado(tipo, tasas)
-mapa = graficar_mapa(coordenadas_aglomerado,colores)
-MAPA_PATH = cons.UTILS_PATH / "mapa_aglomerados.html"
-mapa.save(MAPA_PATH)
-components.html(open(MAPA_PATH, "r", encoding="utf-8").read(), height=600)
-st.divider()
+    tasas = tasa_aglomerado(df)
+    tipo = st.selectbox(
+        "Seleccioná el tipo de tasa que querés visualizar:",
+        ("Seleccionar tasa...", "empleo", "desempleo"),
+    )
+
+    # Si el usuario no eligió nada válido, tipo será None
+    if tipo == "Seleccionar tasa...":
+        tipo = None
+    st.markdown(
+        "<p>- Al elegir la tasa de empleo se ven puntos verdes en los aglomerados cuya tasa de empleo aumentó con el correr del tiempo. Rojo en el caso contrario. </p>"
+        "<p>- Al elegir la tasa de desempleo se ven puntos rojos en los aglomerados cuya"
+        "tasa de empleo aumentó con el correr del tiempo. Verde en el caso contrario. </p>",
+        unsafe_allow_html=True,
+    )
+    if tipo:
+        colores = colores_aglomerado(tipo, tasas)
+        mapa = graficar_mapa(coordenadas_aglomerado,colores)
+        MAPA_PATH = cons.UTILS_PATH / "mapa_aglomerados.html"
+        mapa.save(MAPA_PATH)
+        components.html(open(MAPA_PATH, "r", encoding="utf-8").read(), height=600)
+    st.divider()
 # footer
-footer()
+# footer()
