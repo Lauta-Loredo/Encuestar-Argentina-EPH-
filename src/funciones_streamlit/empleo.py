4216b3551a916ffcdf9d149205ba63673df29962
Ulises Rodriguez <ulises25103@gmail.com>
2025-06-19 21:51:00 -0300

    funciones que no uso borradas en empleo.py

--- src/funciones_streamlit/empleo.py
+++ src/funciones_streamlit/empleo.py
@@ -5,32 +5,6 @@ import folium
 
 ESTADO_LABORAL = "ESTADO"
 
-"""def definir_anio(df,clave):
-    anios_disponibles = sorted(df["ANO4"].unique())
-    anios_opciones = ["Seleccione un año..."] + list(map(int, anios_disponibles))
-
-    anio_seleccionado = st.selectbox("Seleccione un año:", anios_opciones,key=clave)
-
-    if anio_seleccionado != "Seleccione un año...":
-        return anio_seleccionado
-    else:
-        return None
-
-def definir_trimestre(df, anio_seleccionado,clave):
-    if anio_seleccionado is None:
-        return None
-
-    trimestres_disponibles = sorted(df[df["ANO4"] == int(anio_seleccionado)]["TRIMESTRE"].unique())
-    trimestres_opciones = ["Seleccione un trimestre..."] + list(map(int, trimestres_disponibles))
-
-    trimestre_seleccionado = st.selectbox("Seleccione un trimestre:", trimestres_opciones, key=clave) #<- ERROR ENCONTRADO, UTILICE UNA KEY PARA DIFERENCIAR ELEMENTOS SELECTBOX UTILIZANDO DOS FUNCIONES IGUALES
-
-    if trimestre_seleccionado != "Seleccione un trimestre...":
-        return trimestre_seleccionado
-    else:
-        return None"""
-
-
 def muestra_tasa(tasa, evolucion):
     if evolucion:
         fig, ax = plt.subplots()
@@ -56,25 +30,6 @@ def calcular_desocupados_por_nivel(df, anio, trimestre, NIVEL_EDUCATIVO):
     return conteo
 
 # 1.5.2
-"""def definir_aglomerado(df,clave, NOMBRES_AGLOMERADOS):
-    aglomerados_disponibles = sorted(df["AGLOMERADO"].unique())
-    NOMBRES_AGLOMERADOS_INT = {int(key): dato for key, dato in NOMBRES_AGLOMERADOS.items()}
-    # Generar las opciones tipo "2 - Gran La Plata"
-    opciones = [f"{key} - {NOMBRES_AGLOMERADOS_INT.get(key, 'Aglomerado desconocido')}" for key in aglomerados_disponibles]
-    opciones = ["Seleccione un aglomerado..."] + ["Todo el País"] + opciones
-
-    aglomerado_elegido = st.selectbox("Seleccione un aglomerado:", opciones,key=clave)
-
-    if aglomerado_elegido == "Seleccione un aglomerado...":
-        return None
-
-    if aglomerado_elegido == "Todo el País":
-        return "pais"
-
-    # extraer solo la key (esta antes del guion)
-    codigo = int(aglomerado_elegido.split(" - ")[0])
-    return codigo"""
-
 
 def tasa_des_empleo(df, tipo, aglo=None):
     evolucion = {}
